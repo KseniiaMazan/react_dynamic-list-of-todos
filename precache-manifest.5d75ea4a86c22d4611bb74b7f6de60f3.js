e8115de7815de494dca2e6d79d253d57bdcea18f
KseniiaMazan <ksusamazan@gmail.com>
2019-09-25 13:23:03 +0300

    Updates

---
+++ precache-manifest.5d75ea4a86c22d4611bb74b7f6de60f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e783adf468e5bbeb24701fde1fab8ccc",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "6897aac8c7d83b16f4e7",
+    "url": "/react_dynamic-list-of-todos/static/css/main.7e3e5333.chunk.css"
+  },
+  {
+    "revision": "1362b4707a8585be5886",
+    "url": "/react_dynamic-list-of-todos/static/js/2.3b2b4ba5.chunk.js"
+  },
+  {
+    "revision": "6897aac8c7d83b16f4e7",
+    "url": "/react_dynamic-list-of-todos/static/js/main.95e28f58.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  },
+  {
+    "revision": "f99c207000236ee2d00cf3976b72bb02",
+    "url": "/react_dynamic-list-of-todos/static/media/background2.f99c2070.jpg"
+  }
+]);
